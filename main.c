02f384f5e7c0ca6908238b74f020f3d2200dd84f
Gabriel Sardinha <gtbriel.00@gmail.com>
2021-08-13 19:18:05 -0300

    first_step (done)

--- main.c
+++ main.c
@@ -12,22 +12,6 @@
 
 #define BSIZE (2*1024)
 
-/*void makeCopy() {
-	fd_origem = open(<dir_origem>, O_RDONLY);
-	fd_destino = open(<dir_destino>, O_CREAT | O_TRUNC);
-	
-	char bloco[BSIZE];
-	
-	while(1) {
-		n = read(fd_origem, bloco, BSIZE);
-		if(n > 0) {
-			write(fd_destino, bloco, n);
-		}else {
-			break;
-		}
-	}
-	
-}*/
 
 int main(int argc, char **argv) {
 	
@@ -54,25 +38,24 @@ int main(int argc, char **argv) {
 				printf("Fazendo backup de: %s\n",nameList_origem[i]->d_name);
 			}		
 			sleep(3);
-			return 0;
+			continue;
 		}
 		
-		struct stat buffer_origem[n_origem];
-		struct stat buffer_destino[n_destino];
-		
-		for(i=2; i <n_origem;i++) {
-			stat(nameList_origem[i]->d_name, &buffer_origem[i]);
-		}
-		for(i=2;i < n_destino;i++) {
-			stat(nameList_destino[i]->d_name, &buffer_destino[i]);
-		}
+		struct stat buffer_origem;
+		struct stat buffer_destino;
 		
 		for(i=2;i < n_origem;i++) {
-			int foiEncontrado = 0;				
+			int foiEncontrado = 0;							
 			for(j=2;j < n_destino;j++) {
 				if(strcmp(nameList_origem[i]->d_name, nameList_destino[j]->d_name) == 0) {
 					foiEncontrado = 1;
-					if(buffer_origem[i].st_mtime > buffer_destino[j].st_mtime) {
+					char path_destino[200] = "minha_pasta_backup/";
+					strcat(path_destino, nameList_destino[j]->d_name);
+					char path_origem[200] = "minha_pasta/";
+					strcat(path_origem, nameList_origem[i]->d_name);
+					stat(path_origem, &buffer_origem);
+					stat(path_destino, &buffer_destino);
+					if(buffer_origem.st_mtime > buffer_destino.st_mtime) {
 						printf("(Arquivo desatualizado) Fazendo backup de: %s\n", nameList_origem[i]->d_name);
 						break;
 					}
